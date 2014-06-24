c99953d2ad89d0fbba3b6c4c5d4f6df781154075
Partha Sarma <ambition.logic@gmail.com>
2014-06-24 15:15:58 +0530

    added battleship.js file

---
+++ js/battleship.js
@@ -0,0 +1,14 @@
+//To hold the location of each cell of the ship
+var location1 = 0, location2 = 0, location3 = 0;
+
+//To hold the user guess
+var guess = 0;
+
+//to hold the number of hits
+var hits = 0;
+
+//to hold the number of guesses
+var guesses = 0;
+
+//to hold the status of the ship (sunk or not)
+var isSunk = false;

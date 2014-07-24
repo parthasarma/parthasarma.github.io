b436834f5d5dcbe721111530bc01b0c0af007236
Partha Sarma <ambition.logic@gmail.com>
2014-07-24 16:39:49 +0530

    Hello

--- js/battleship.js
+++ js/battleship.js
@@ -73,30 +73,36 @@ var controller = {
 		var location = parseGuess(guess);
 
 		if (location) {
-			
-		}
-	},
-
-	parseGuess: function (guess) {
-		var alphabet = ["A", "B", "C", "D", "E", "F"];
-
-		if (guess === null || guess.length !== 2) {
-			alert("Oops, please enter a letter and a number on the board.");
-		}else{
-			firstChar = guess.charAt(0);
-
-			var row = alphabet.indexOf(firstChar);
-			var column = guess.charAt(1);
-
-			if (isNaN(row) || isNaN(column)) {
-				alert("Oops, that isn't on the board.");
-			}else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
-				alert("Oops, that's off the board!");
-			}else{
-				return row + column;
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipSunk === model.numShips) {
+				view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses.");
 			}
 		}
-
-		return null;
 	}
-};
+
+}
+
+
+function parseGuess (guess) {
+	var alphabet = ["A", "B", "C", "D", "E", "F"];
+
+	if (guess === null || guess.length !== 2) {
+		alert("Oops, please enter a letter and a number on the board.");
+	}else{
+		var firstChar = guess.charAt(0);
+
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+
+		if (isNaN(row) || isNaN(column)) {
+			alert("Oops, that isn't on the board.");
+		}else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+			alert("Oops, that's off the board!");
+		}else{
+			return row + column;
+		}
+	}
+
+	return null;
+}

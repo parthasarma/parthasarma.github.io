61578a57379b37da8882f202f148583e01c07cb9
Partha Sarma <ambition.logic@gmail.com>
2014-07-25 15:46:45 +0530

    added code to handle IE9 or earlier

--- js/battleship.js
+++ js/battleship.js
@@ -125,6 +125,11 @@ function handleFireButton () {
 
 function handleKeyPress (e) {
 	var fireButton = document.getElementById("fireButton");
+
+	// in IE9 and earlier, the event object doesn't get passed
+	// to the event handler correctly, so we use window.event instead.
+	e = e || window.event;
+	
 	if (e.keyCode === 13) {
 		fireButton.click();
 		return false;
